d26e9769c217011f061667af3860b321914afd3b
Emily Turner <75638995+Emily-Turner996@users.noreply.github.com>
2023-10-26 11:23:18 -0400

    Fixed an overlooked issue with numBallots function
    I somehow overlooked the fact that the result of numBallots wasn't always accurate, then realized I needed to update ballotArray along with the text file

--- employeeVoting.py
+++ employeeVoting.py
@@ -10,8 +10,10 @@ because I plan on making additions to the program as a whole and this will make 
 when the time comes. 
 
 Function prompts user for their ID and who they want to vote for, and appends it to the file unless they've already voted.
+
+Function takes ballotArray because that array must be updated when new votes come in for the accuracy of other functions
 """
-def newVote():
+def newVote(ballotArray):
     userID = input("Please enter the 4-digit pin you use to clock in: ")  # prompt user for their ID
 
     # checking to see if the voter has voted already
@@ -19,6 +21,7 @@ def newVote():
         vote = input("Who do you think should win employee of the month?: ")  # prompt user for their vote
         ballot = (userID + " " + vote + "\n")  # saving information to be appended in a single line for formatting & testing
         votes.write(ballot)  # append the vote to the text file
+        ballotArray.append([userID, vote]) # need to update ballotArray here so that the correct number of votes can be counted
         print("Vote for " + vote + " saved successfully.")
 
     elif checkUserID(userID) == False:
@@ -47,13 +50,20 @@ def checkUserID(userID):
     return newID # return the correct boolean
 
 
-
 """
 
+"""
+#def confirmVote(vote): # you totally don't see this yet
+    
+
+
+
+"""
+numBallots(ballotArray) - Function to calculate and display the number of people who have voted.
 """
 def numBallots(ballotArray):
-    ballotCount = str(len(ballotArray))
-    print("Total number of votes saved: " + ballotCount)
+    ballotCount = str(len(ballotArray)) # calculate the number of votes
+    print("Total number of votes saved: " + ballotCount) # display results
 
 
 # Setting up file for use:
@@ -65,8 +75,8 @@ ballotArray = votes.readlines() # save contents of text file into an array, spli
 
 
 # Main Program:
-newVote() # Function that allows user to vote
-numBallots(ballotArray)
+newVote(ballotArray) # Function that allows user to vote
+numBallots(ballotArray) # Function that calculated and display the number of votes so far
 
 
 
